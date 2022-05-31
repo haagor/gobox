a8aa65afef5d7cdb044ff43cdcaae348eef69163
haagor <sim.paris06@gmail.com>
2022-05-31 10:41:46 +0200

    update README

--- src/entities/tests/persona_test.go
+++ src/entities/tests/persona_test.go
@@ -1,20 +1,20 @@
 package tests
 
-
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
-    "github.com/haagor/gobox/src/entities"
+	"testing"
+
+	"github.com/haagor/gobox/src/entities"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGiveMoney(t *testing.T) {
-    p1 := entities.NewPersona()
-    p1.Inventory.Money = 10
-    p2 := entities.NewPersona()
+	p1 := entities.NewPersona()
+	p1.Inventory.Money = 10
+	p2 := entities.NewPersona()
 
-    p1.GiveMoney(p2, 10)
+	p1.GiveMoney(p2, 10)
 
-    assert.Equal(t, p1.Inventory.Money, 0)
-    assert.Equal(t, p2.Inventory.Money, 10)
+	assert.Equal(t, p1.Inventory.Money, 0)
+	assert.Equal(t, p2.Inventory.Money, 10)
 
 }
