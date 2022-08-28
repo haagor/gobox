6b23c7fae9bd521588429b63a05b4e511e70399d
haagor <sim.paris06@gmail.com>
2022-08-28 13:11:11 +0200

    v1 next gen

--- katas/gameOfLife/game_of_life.go
+++ katas/gameOfLife/game_of_life.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+    "bufio"
+    "fmt"
+    "log"
     "os"
     "strings"
 )
@@ -13,6 +16,7 @@ func check(e error) {
 
 func main() {
     initWorld(8, 16)
+    nextGen("current_gen.txt")
 }
 
 func initWorld(x, y int) {
@@ -28,5 +32,103 @@ func initWorld(x, y int) {
     for i := 1; i < x; i++ {
         _, err = f.WriteString(line)
         check(err)
+    }
+}
+
+func nextGen(file string) {
+    nextGen := ""
+
+    world := convertWorldFile(file)
+    for x, line := range world {
+        for y, _ := range line {
+            nextGen += evalCell(world, x, y)
+        }
+        nextGen += "\n"
+    }
+    fmt.Println(nextGen)
+}
+
+func convertWorldFile(file string) (res [][]string) {
+    f, err := os.Open(file)
+    check(err)
+    defer f.Close()
+
+    scanner := bufio.NewScanner(f)
+
+    i := 0
+    for scanner.Scan() {
+        line := strings.TrimSuffix(scanner.Text(), "\n")
+        res = append(res, []string{})
+        for _, s := range line {
+            res[i] = append(res[i], string(s))
+        }
+        i++
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    return res
+}
+
+func evalCell(w [][]string, x int, y int) string {
+    liveCell := 0
+
+    if x > 0 && y > 0 {
+        if w[x-1][y-1] == "*" {
+            liveCell++
+        }
+    }
+    if x > 0 {
+        if w[x-1][y] == "*" {
+            liveCell++
+        }
+    }
+    if x > 0 && y < len(w[0])-2 {
+        if w[x-1][y+1] == "*" {
+            liveCell++
+        }
+    }
+    if y > 0 {
+        if w[x][y-1] == "*" {
+            liveCell++
+        }
+    }
+    if y < len(w[0])-2 {
+        if w[x][y+1] == "*" {
+            liveCell++
+        }
+    }
+    if x < len(w[0][0])-2 && y > 0 {
+        if w[x+1][y-1] == "*" {
+            liveCell++
+        }
+    }
+    if x < len(w[0][0])-2 {
+        if w[x+1][y] == "*" {
+            liveCell++
+        }
+    }
+    if x < len(w[0][0])-2 && y < len(w[0])-2 {
+        if w[x+1][y+1] == "*" {
+            liveCell++
+        }
+    }
+
+    if w[x][y] == "." {
+        if liveCell == 3 {
+            return "*"
+        } else {
+            return "."
+        }
+    } else {
+        if liveCell < 2 {
+            return "."
+        } else if liveCell < 4 {
+            return "*"
+        } else {
+            return "."
+        }
     }
 }
